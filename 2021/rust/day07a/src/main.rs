6cfa27689429b5577cacc7f9a54258542a7f8eaa
haochuan <haochuan.liu@gmail.com>
2022-05-02 05:19:15 -0700

    day 7 done

---
+++ 2021/rust/day07a/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let num_list = include_str!("../input.txt")
+        .trim()
+        .split(",")
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let min_fuel = (0..=*num_list.iter().max().unwrap())
+        .map(|n| num_list.iter().map(|t| (n - t).abs()).sum::<i32>())
+        .min()
+        .unwrap();
+
+    println!("min fuel needed is {}", min_fuel);
+}
